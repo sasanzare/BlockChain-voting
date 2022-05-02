4e5acefe63a5cc29d16c30050098a983cba8721d
sasanzare <sasanzare75@gmail.com>
2022-05-02 18:35:29 +0430

    Functions definition

--- voting.sol
+++ voting.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: GPLV3
+// SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.4.0 <0.9.0;
 
 contract Voting {
@@ -10,9 +10,9 @@ contract Voting {
     }
 
     mapping(uint=>Candidate)public Candidates;
-    mapping(address=>Candidate)Participants;
+    mapping(address=>bool)Participants;
 
-    uint128 CondidateCount;
+    uint128 public CandidateCount;
     address owner;
     
     constructor(){
@@ -20,12 +20,30 @@ contract Voting {
     }
 
     function AddCandidate(string memory name)public returns (string memory) {
-        
+        require(msg.sender==owner,"You can not add a candidate!");
+        CandidateCount++;
+        Candidates[CandidateCount]=Candidate(CandidateCount,name,0);
+        return "Candidate added.";
     }
+
     function Vote(uint id)public returns (string memory) {
-        
+        require(id<=CandidateCount && id>0,"No candidate found!");
+        require(Participants[msg.sender]== false,"You have already voted!");
+        Candidates[id].VoteCount++;
+        Participants[msg.sender]=true;
+        return "Thank you for your vote.";
     }
+
     function ShowWinner()view public returns (string memory) {
-        
+        uint winnerId =0;
+        uint winnerVote=0;
+
+        for(uint i =1;i<=CandidateCount;i++){
+            if (Candidates[i].VoteCount>=winnerVote) {
+                winnerId=i;
+                winnerVote=Candidates[i].VoteCount;
+            }
+        }
+        return Candidates[winnerId].name;
     }
 }
